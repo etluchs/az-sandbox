6b59322fbc868aa40fd2fe8d3895ec280182bb4c
Marcus Schulte <marcus.schulte@meteoswiss.ch>
2025-08-12 11:11:58 +0200

    repair

---
+++ webdoc/render-presentations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pushd $(dirname "$0")
+mkdir -p build
+cp src/css/* build
+cp src/presentations/*.??g target
+
+for presi in src/presentations/*.adoc; do
+  arg=${presi##src/presentations/}
+  echo "rendering $presi"
+
+  docker run -u $(id -u) --rm -v `pwd`/src/presentations:/documents/ -v `pwd`/build:/target/ \
+              asciidoctor/docker-asciidoctor \
+              asciidoctor-revealjs $arg --safe-mode=unsafe -D /target
+done
+popd
